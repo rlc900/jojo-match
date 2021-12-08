df1d63c91a0c6413f03814b2df0f7705871dfc98
Rianna <rianna.cleary@gmail.com>
2021-12-08 14:54:59 -0500

    card component created

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Card from './Components/Card';
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </h1>
+        <Card></Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Card.js
@@ -0,0 +1,11 @@
+function Card() {
+    return (
+      <div className="Card">
+          <h1>
+            Card Component
+          </h1>
+      </div>
+    );
+  }
+  
+  export default Card;
