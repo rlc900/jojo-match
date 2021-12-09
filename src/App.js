b079c0656dde50d1ad102f02b8155ca257954528
Rianna <rianna.cleary@gmail.com>
2021-12-08 20:05:09 -0500

    rendered multiple cards

--- src/App.js
+++ src/App.js
@@ -7,6 +7,39 @@ import React, { useState } from 'react';
 
 function App() {
 
+  const jojoArr = [
+    {
+      id: 1,
+      name: 'Jotaro',
+      img: './images/jojo_pony.png'
+    },
+    {
+      id: 2,
+      name: 'Jonathan',
+      img: './images/jonathan.png'
+    },
+    {
+      id: 3,
+      name: 'Joseph',
+      img: './images/joseph.png'
+    },
+    {
+      id: 4,
+      name: 'Josuke',
+      img: './images/josuke.png'
+    },
+    {
+      id: 5,
+      name: 'Giorno',
+      img: './images/giorno.png'
+    },
+    {
+      id: 6,
+      name: 'Jolyne',
+      img: './images/jolyne.png'
+    }
+  ]
+
   const [isFlipped, setIsFlipped] = useState(false)
 
   function handleClick () {
@@ -14,19 +47,19 @@ function App() {
   }
 
   return (
-  
-        
-        <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+    <div>
+      {jojoArr.map((jojo) => {
+        return <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
         <FrontCard onClick={handleClick}>
-          <Logo src='./images/jojo.png'/>
+          <Logo src='./images/jojo.png' alt='logo'/>
         
         </FrontCard>
         <BackCard onClick={handleClick}>
-          <Character src='./images/jojo_pony.png' alt='Zaddy'/>
+          <Character src={jojo.img} alt='Jojo'/>
         </BackCard>
-
         </ReactCardFlip>
-
+      })}
+        </div>
   );
 }
 
