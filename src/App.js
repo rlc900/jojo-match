75407f6562f3c980801f37b800e630830f08e8eb
Rianna <rianna.cleary@gmail.com>
2021-12-14 19:38:53 -0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
 // import styled from 'styled-components'
 import './App.css';
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 import Card from './Components/Card';
 
-function App() {
+const jojoArr = [
+  {
+    img: './images/jojo_pony.png'
+  },
+  {
+    img: './images/jonathan.png'
+  },
+  {
+    img: './images/joseph.png'
+  },
+  {
+    img: './images/josuke.png'
+  },
+  {
+    img: './images/giorno.png'
+  },
+  {
+    img: './images/jolyne.png'
+  }
+]
 
+function App() {
+const [cards, setCards] = useState([])
+const [turns, setTurns] = useState(0)
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...jojoArr, ...jojoArr]
+    .sort(() => Math.random() - 0.5)
+    .map((card) => ({...card, id: Math.random()}));
+
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  console.log(cards, turns)
 
   return (
     <div>
-      <button>
+      <button onClick={shuffleCards}>
         New Game
       </button>
       <Card/>
